360a63232dfaa402bede037cbb4e12bb7bb6a39d
nestor-c <nestord.cortes@gmail.com>
2019-01-17 13:54:24 -0800

    Didn't add much. Finished the createClassroom function.

--- Sketch.js
+++ Sketch.js
@@ -1,7 +1,7 @@
 import { Rectangle } from "./Rectangle.js";
 
 // import {handleClientLoad} from './gAPI.js'
-
+//TODO: Make an easy to implement classroom creator
 let cRooms = [];
 const WIDTH = window.innerWidth;
 const HEIGHT = window.innerHeight;
@@ -10,13 +10,79 @@ function setup() {
 	var myCanvas = createCanvas(HEIGHT, WIDTH);
 	myCanvas.parent("canvas");
 	
-	for (let i=0; i<5; i++){
-		cRooms.push(new Rectangle(300,300,200,200));
-	}
-	
+	cRooms = createClassRooms(4);
 	// window.onload = handleClientLoad(cRooms);
 }
 
+/**
+ * @param {num} num The number of classrooms to create;
+ * @param {[[h,w],...]} dim Specify dimensions for classrooms via a 2D array.
+ * @example dim - [[200,200][300,300]]
+ * @param {[[x,y],...]} coord Specifies coordinates for classrooms via a 2D array.
+ * @example coord - [[300,200][500,300]]
+ */
+function createClassRooms(num,defaultSize,dim,coord){
+	if (num === undefined){
+		console.log("Error: num not specified")
+		return -1;
+	}
+	let rooms = [];
+	let randX, randY;
+	if (dim === undefined && coord === undefined){
+		for(let i=0;i < num; i++){
+			randX=Math.floor(Math.random() * WIDTH);
+			randY=Math.floor(Math.random() * HEIGHT);
+			rooms.push(new Rectangle(randX,randY,defaultSize,defaultSize))
+		}
+	} 
+	else if (dim != undefined && coord === undefined) {
+		if (dim.length != num){
+			throw("Dim array length must match num. Leave empty arrays if you don't plan on specifying all.")
+		}
+		for (let i=0; i<num; i++){
+			if (dim[i].length != 0 && dim[i].length === 2){ 
+				randX=Math.floor(Math.random() * WIDTH);
+				randY=Math.floor(Math.random() * HEIGHT);
+				rooms.push(new Rectangle(randX,randY,dim[i][0],dim[i][1]));
+			}
+			else if (dim[i].length != 2){
+				throw("Dim array: "+ i +" should have length 2.[h,w]")
+			}
+			randX=Math.floor(Math.random() * WIDTH);
+			randY=Math.floor(Math.random() * HEIGHT);
+			rooms.push(new Rectangle(randX,randY,defaultSize,defaultSize))
+		}
+	}
+	else if (dim === undefined && coord != undefined) {
+		if (coord.length != num){
+			throw("Coord array length must match num. Leave empty arrays if you don't plan on specifying all.")
+		}
+		for (let i=0; i<num; i++){
+			if (coord[i].length != 0 && coord[i].length === 2) rooms.push(new Rectangle(coord[i][0],coord[i][1],200,200));
+			else if (coord[i].length != 2){
+				throw("Coord array: "+ i +" should have length 2.[x,y]")
+			}
+			randX=Math.floor(Math.random() * WIDTH);
+			randY=Math.floor(Math.random() * HEIGHT);
+			rooms.push(new Rectangle(randX,randY,200,200))
+		}
+	}
+	else if (dim != undefined && coord != undefined){
+		if (coord.length != num && dim.length != num){
+			throw("Coord && dim array length must match num. Leave empty arrays if you don't plan on specifying all.")
+		}
+		for (let i=0; i<num; i++){
+			if (coord[i].length === 2 && dim[i].length === 2) rooms.push(new Rectangle(coord[i][0],coord[i][1],dim[i][0],dim[i][1]));
+			else if (coord[i].length != 2 || coord[i].length != 0){
+				throw("Coord array: "+ i +" should have length 2 or be empty.[x,y]")
+			}
+			randX=Math.floor(Math.random() * WIDTH);
+			randY=Math.floor(Math.random() * HEIGHT);
+			rooms.push(new Rectangle(randX,randY,200,200))
+		}
+	}
+	return rooms;
+}
 function draw() {
 	let color = "#b7bfcc";
 	background(color)
@@ -24,14 +90,12 @@ function draw() {
 		cRooms[i].display('classroom' + i);
 		cRooms[i].checkAndToggleMouseOver();
 		cRooms[i].move();
-		for (let j = i+1; j < cRooms.length; j++) {
+		for (let j = 0; j < cRooms.length; j++) {
 			cRooms[i].snapTogether(cRooms[j]);
 		}
 	}
 }
-
 function mousePressed() {
-	let shallow = shallowest(cRooms);
 	cRooms.forEach(room => {
 		if (room.mouseOver) {
 			room.toggleDragging();
@@ -40,7 +104,6 @@ function mousePressed() {
 			room.offsetY = room.Y - mouseY;
 		}
 	})
-
 }
 function mouseReleased() {
 	cRooms.forEach(room => {
@@ -49,35 +112,7 @@ function mouseReleased() {
 		}
 	})
 }
-function shallowest(rooms){
-	//COND: We only need the rooms that are intersecting.
-	// Might also only want to grab those that the mouse is clicking on.
-	if (rooms.length >= 2) {
-		let shallowest = rooms[0];
 
-		rooms.forEach(rm => {
-			if (rm.depth < shallowest.depth) {
-				shallowest = rm;
-			}
-		});
-		return shallowest;
-	}
-}
-function stackedClassrooms(cRooms) {
-	//COND: Check which classrooms are intersecting.
-	// Return an array of all intersecting classrooms.
-	if (cRooms.lenght >= 2) {
-		var stacked = [];
-		for (let i = 0; i < cRooms.length - 1; i++) {
-			for (let j = i + 1; j < cRooms.length; j++) {
-				if (cRooms[i]._intersecting(j)) {
-					stacked.push(cRooms[i]);
-					stacked.push(cRooms[j]);
-				}
-			}
-		}
-	}
-}
 window.setup = setup;
 window.onload = window.setup
 window.draw = draw;
