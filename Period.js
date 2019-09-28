2840e5094293e4f7b97de8251b801e42490e1710
nestor-c <nestord.cortes@gmail.com>
2019-09-27 19:04:33 -0700

    Adding new schedule object

--- Period.js
+++ Period.js
@@ -1,11 +1,49 @@
-var displayPeriod = document.getElementById("Period");
+var periodDisplay = document.getElementById("Period");
+let pd_Text = displayPeriod.firstChild.textContent;
+let periods = [];
+let schedule = 
+[
+    { "Regular" : 
+        [
+            {1:[[7,40],[8,28]]},
+            {2:[[8,32],[9,20]]},
+            {3:[[9,24],[10,12]]},
+            {4:[[10,16],[11,04]]},
+            [   
+                {"First Lunch":[[11,04],[11,34]]},
+                {5:[[11,38],[12,26]]},
+                {"Second Lunch":[[11,56],[12,26]]},
+                {5:[[11,08],[11,56]]}
+            ],
+            {6:[[12,30],[1,18]]},
+            {7:[[1,22],[2,11]]} 
+        ]
+    },
+    {  "Minimum" : 
+        [
+            {1:[[7,40],[8,28]]},
+            {2:[[8,32],[9,20]]},
+            {3:[[9,24],[10,12]]},
+            {4:[[10,16],[11,04]]},
+            {5:[[11,04],[11,34]]},
+            {6:[[12,30],[1,18]]},
+            {7:[[1,22],[2,11]]} 
+        ]
+    }
+]
 
-let periods = new Date();
-periods.setHours(7);
-periods.setMinutes(35);
+let HOUR = function(time){
+    switch (time){
+        case 1:
+        case 2:
+        case 3:
+        case 4:
+        case 5:
+        case 6:
+        default:
+    }
+}
 
 let now = new Date();
 
-if (now.getHours() < periods.getHours() || (now.getHours() <= periods.getHours() && now.getMinutes() < periods.getMinutes)){
-    displayPeriod.firstChild.textContent = "Before first";
-}
+
